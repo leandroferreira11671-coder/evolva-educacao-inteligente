d230a22e5dd51b0f23413fb7ce5a9c0b8a2734b6
leandroferreira11671-coder <leandroferreira11671@gmail.com>
2026-03-03 22:24:28 -0300

    Update portugues-controller.js

--- portugues/portugues-controller.js
+++ portugues/portugues-controller.js
@@ -1,1 +1,31 @@
+import { fonologiaBasico } from "./português/Básico/fonologia.js";
 
+/* BANCO DE PERGUNTAS */
+
+window.bancoPerguntas = {
+
+basico:{
+
+fonologia:fonologiaBasico,
+
+morfologia:[],
+
+sintaxe:[],
+
+semantica:[]
+
+},
+
+avancado:{
+
+fonologia:[],
+
+morfologia:[],
+
+sintaxe:[],
+
+semantica:[]
+
+}
+
+};

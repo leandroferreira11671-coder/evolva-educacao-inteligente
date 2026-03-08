3d62ad26efd52caf3088053f7134d0833e5a7e44
leandroferreira11671-coder <leandroferreira11671@gmail.com>
2026-03-08 16:34:36 -0300

    Update controller.js

--- materia/portugues/controller.js
+++ materia/portugues/controller.js
@@ -1,1 +1,19 @@
+function abrirAula(aula){
 
+    if(aula === "fonologia"){
+        window.location.href = "basico/fonologia.html"
+    }
+
+    if(aula === "morfologia"){
+        window.location.href = "basico/morfologia.html"
+    }
+
+    if(aula === "sintaxe"){
+        window.location.href = "basico/sintaxe.html"
+    }
+
+    if(aula === "semantica"){
+        window.location.href = "basico/semantica.html"
+    }
+
+}
